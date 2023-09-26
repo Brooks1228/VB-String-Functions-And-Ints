e62e78f656575b779abff833eed0f988fea7021f
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2023-09-26 09:33:01 -0400

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    'Brooks Keller
+    '9/26/2023
+    'basic string functions and integers
+    Sub Main()
+        Dim input As String
+        Dim city, state As String
+        Dim commaLocation, stringLength As Integer
+
+        Console.Write("Please enter a city and state separated by only a comma (no spaces) >> ")
+        input = Console.ReadLine
+
+        'our goal is so separate this into two different string variables (city and state)
+        'we assume we have a string in the format city,state
+
+        'find the comma
+        commaLocation = input.IndexOf(",") 'find the comma !!!
+        Console.WriteLine("The comma is located at position {0}", commaLocation)
+        stringLength = input.Length
+        Console.WriteLine("The length of the string is {0}", stringLength)
+
+        'take the first part of the string up to the comma
+        city = input.Substring(0, commaLocation)
+        Console.WriteLine("The city is {0}", city)
+
+        'take the last part of the string after the comma, that is the state
+        state = input.Substring(commaLocation + 1, stringLength - commaLocation - 1)
+        Console.WriteLine("The state is {0}", state)
+    End Sub
+
+End Module
---
+++ stringReplacements.vb
@@ -0,0 +1,17 @@
+﻿Module stringReplacements
+    'Brooks Keller
+    '9/26/2023
+    'string replacements
+    Sub main()
+        Dim input As String
+        Dim newString As String
+        Dim originalString As String = "Mr. Klins is the teacher of this class"
+
+        Console.WriteLine(originalString)
+        Console.Write("What should we change Mr. Klins' name to? >> ")
+        input = Console.ReadLine
+        newString = originalString.Replace("Mr. Klins", input) 'STRING REPLACEMENT HERE LOOK LOOOOOOOK
+        Console.WriteLine("The new string is {0} characters long and is as follows: ", newString.Length)
+        Console.WriteLine(newString)
+    End Sub
+End Module
